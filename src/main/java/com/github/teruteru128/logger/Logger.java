edc71b03dae51219af88f55f282797efa1292fd1
snowpegeon <guitarcat0514@gmail.com>
2024-04-11 21:36:25 +0900

    Logger　 コード成型

--- src/main/java/com/github/teruteru128/logger/Logger.java
+++ src/main/java/com/github/teruteru128/logger/Logger.java
@@ -1,70 +1,71 @@
 package com.github.teruteru128.logger;
 
-import org.apache.logging.log4j.Level;
-import org.bukkit.plugin.Plugin;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.logging.log4j.Level;
+import org.bukkit.plugin.Plugin;
 
 
 public class Logger {
-    private static final Map<Plugin, Logger> map = Collections.synchronizedMap(new HashMap<>());
-    private final java.util.logging.Logger _logger;
-    private final Level configuredLevel;
 
-    Logger(java.util.logging.Logger logger, String level) {
-        this(logger, Level.toLevel(level, Level.INFO));
-    }
+  private static final Map<Plugin, Logger> map = Collections.synchronizedMap(new HashMap<>());
+  private final java.util.logging.Logger _logger;
+  private final Level configuredLevel;
 
-    Logger(java.util.logging.Logger logger, Level level) {
-        this._logger = logger;
-        this.configuredLevel = level;
-    }
+  Logger(java.util.logging.Logger logger, String level) {
+    this(logger, Level.toLevel(level, Level.INFO));
+  }
 
-    public static void register(Plugin plugin, String logLevel) {
-        map.put(plugin, new Logger(plugin.getLogger(), logLevel));
-    }
+  Logger(java.util.logging.Logger logger, Level level) {
+    this._logger = logger;
+    this.configuredLevel = level;
+  }
 
-    public static Logger getInstance(Plugin plugin) {
-        return map.get(plugin);
-    }
+  public static void register(Plugin plugin, String logLevel) {
+    map.put(plugin, new Logger(plugin.getLogger(), logLevel));
+  }
 
-    public static void unregister(Plugin plugin) {
-        map.remove(plugin);
-    }
+  public static Logger getInstance(Plugin plugin) {
+    return map.get(plugin);
+  }
 
-    public void fatal(String msg) {
-        this.log(Level.FATAL, java.util.logging.Level.SEVERE, msg);
-    }
+  public static void unregister(Plugin plugin) {
+    map.remove(plugin);
+  }
 
-    public void error(String msg) {
-        this.log(Level.ERROR, java.util.logging.Level.SEVERE, msg);
-    }
+  public void fatal(String msg) {
+    this.log(Level.FATAL, java.util.logging.Level.SEVERE, msg);
+  }
 
-    public void warn(String msg) {
-        this.log(Level.WARN, java.util.logging.Level.WARNING, msg);
-    }
+  public void error(String msg) {
+    this.log(Level.ERROR, java.util.logging.Level.SEVERE, msg);
+  }
 
-    public void info(String msg) {
-        this.log(Level.INFO, java.util.logging.Level.INFO, msg);
-    }
+  public void warn(String msg) {
+    this.log(Level.WARN, java.util.logging.Level.WARNING, msg);
+  }
 
-    public void debug(String msg) {
-        this.log(Level.DEBUG, java.util.logging.Level.INFO, msg);
-    }
+  public void info(String msg) {
+    this.log(Level.INFO, java.util.logging.Level.INFO, msg);
+  }
 
-    public void trace(String msg) {
-        this.log(Level.TRACE, java.util.logging.Level.INFO, msg);
-    }
+  public void debug(String msg) {
+    this.log(Level.DEBUG, java.util.logging.Level.INFO, msg);
+  }
 
-    public void log(org.apache.logging.log4j.Level viewLevel, java.util.logging.Level logLevel, String msg) {
-        if (viewLevel.compareTo(this.configuredLevel) >= 0) {
-            this._logger.log(logLevel, msg);
-        }
-    }
+  public void trace(String msg) {
+    this.log(Level.TRACE, java.util.logging.Level.INFO, msg);
+  }
 
-    public java.util.logging.Logger getLogger() {
-        return this._logger;
+  public void log(org.apache.logging.log4j.Level viewLevel, java.util.logging.Level logLevel,
+      String msg) {
+    if (viewLevel.compareTo(this.configuredLevel) >= 0) {
+      this._logger.log(logLevel, msg);
     }
+  }
+
+  public java.util.logging.Logger getLogger() {
+    return this._logger;
+  }
 }
--- src/main/java/com/github/teruteru128/logger/Main.java
+++ src/main/java/com/github/teruteru128/logger/Main.java
@@ -3,4 +3,5 @@ package com.github.teruteru128.logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
 }
